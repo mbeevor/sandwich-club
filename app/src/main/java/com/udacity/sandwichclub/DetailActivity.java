703e4e111be23301a89075e680d999fdae52bece
Matthew Beevor <mbeevor@gmail.com>
2018-03-07 21:26:47 +0000

    For submission

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,24 +3,46 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    Sandwich sandwich;
+    public ImageView sandwichIv;
+    public TextView descriptionLabelTv;
+    public TextView descriptionTv;
+    public TextView alsoKnownAsLabelTv;
+    public TextView alsoKnownAsTv;
+    public TextView ingredientsLabelTv;
+    public TextView ingredientsTv;
+    public TextView originLabelTv;
+    public TextView originTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        sandwichIv = findViewById(R.id.image_iv);
+        descriptionLabelTv = findViewById(R.id.description_label_tv);
+        descriptionTv = findViewById(R.id.description_tv);
+        alsoKnownAsLabelTv = findViewById(R.id.also_known_label_tv);
+        alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        ingredientsLabelTv = findViewById(R.id.ingredients_label_tv);
+        ingredientsTv = findViewById(R.id.ingredients_tv);
+        originLabelTv = findViewById(R.id.origin_label_tv);
+        originTv = findViewById(R.id.origin_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,7 +58,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -46,7 +68,7 @@ public class DetailActivity extends AppCompatActivity {
         populateUI();
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(sandwichIv);
 
         setTitle(sandwich.getMainName());
     }
@@ -57,6 +79,42 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+
+        // Set description using data from Json, hiding label and TextView if empty
+        if (sandwich.getDescription().isEmpty()) {
+            descriptionLabelTv.setVisibility(View.GONE);
+            descriptionTv.setVisibility(View.GONE);
+        } else {
+            descriptionTv.setText(sandwich.getDescription());
+        }
+
+        // Set place of origin using data from Json, hiding label and TextView if empty
+        if (sandwich.getPlaceOfOrigin().isEmpty()) {
+            originLabelTv.setVisibility(View.GONE);
+            originTv.setVisibility(View.GONE);
+        } else {
+            originTv.setText(sandwich.getPlaceOfOrigin());
+        }
+
+        /* Set "also known as" using data from Json, using TextUtils.join
+        *to separate values with ","; hiding label and TextView if empty
+         */
+        if (sandwich.getAlsoKnownAs().isEmpty()) {
+            alsoKnownAsTv.setVisibility(View.GONE);
+            alsoKnownAsLabelTv.setVisibility(View.GONE);
+        } else {
+            alsoKnownAsTv.setText(android.text.TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+        }
+
+        /* Set ingredients using data from Json, using TextUtils.join
+        *to separate values with ","; hiding label and TextView if empty
+         */
+        if (sandwich.getIngredients().isEmpty()) {
+            ingredientsTv.setVisibility(View.GONE);
+            ingredientsLabelTv.setVisibility(View.GONE);
+        } else {
+            ingredientsTv.setText(android.text.TextUtils.join(", ", sandwich.getIngredients()));
+        }
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,79 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    // constants for key strings in JSON
+    public static final String NAME = "name";
+    public static final String MAIN_NAME = "mainName";
+    public static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    public static final String INGREDIENTS = "ingredients";
+    public static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String DESCRIPTION = "description";
+    public static final String IMAGE = "image";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        JSONObject jsonObject;
+        String mainName;
+        List<String> alsoKnownAs = new ArrayList<>();
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients = new ArrayList<>();
+
+        try {
+
+            //instantiate JSONObject
+            jsonObject = new JSONObject(json);
+
+            //Find and assign sandwich name
+            JSONObject nameJsonObject = jsonObject.getJSONObject(NAME);
+            mainName = nameJsonObject.optString(MAIN_NAME);
+
+            // Assign remaining objects with values
+            placeOfOrigin = jsonObject.optString(PLACE_OF_ORIGIN);
+            description = jsonObject.optString(DESCRIPTION);
+            image = jsonObject.optString(IMAGE);
+
+            // Use JSONArray for also-known-as name
+            JSONArray alsoKnownAsArray = nameJsonObject.getJSONArray(ALSO_KNOWN_AS);
+            for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                if (alsoKnownAsArray != null) {
+                    String alsoKnownAsString = alsoKnownAsArray.optString(i);
+                    alsoKnownAs.add(alsoKnownAsString);
+                }
+
+            }
+
+            // Use JSONArray for list of ingredients
+            JSONArray ingredientsArray = jsonObject.getJSONArray(INGREDIENTS);
+            for (int j = 0; j < ingredientsArray.length(); j++) {
+                String ingredientsString = ingredientsArray.optString(j);
+                ingredients.add(ingredientsString);
+            }
+
+            // return new sandwich using above values in constructor
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin,
+                    description, image, ingredients);
+
+            // catch exception, and print error to log
+        } catch (final JSONException e) {
+            Log.e("JsonUtils", "Error in Json", e);
+            // return 'null' if try/catch fails
+            return null;
+
+        }
+
     }
 }
